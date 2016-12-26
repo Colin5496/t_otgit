65b0043df6b1829e55f8e3df2ce6c68262c20c16
namsoo <ukun3147@naver.com>
2016-12-26 15:33:07 +0900

    conflict resolved

--- sangheon.js
+++ sangheon.js
@@ -2,7 +2,7 @@ console.log('my name is nayoung');
 console.log('my name is namyee');
 console.log("안녕하세요 이상헌 입니다.");
 console.log("저는 세종대학교에 재학 중 입니다.");
-<<<<<<< HEAD
+
 console.log('my name is namyee');
 
 
@@ -11,5 +11,3 @@ int void()
 {
 console.log('비상비상비상 충돌충돌 아니면 말구');
 }
-=======
->>>>>>> eec734c6e176e50c5625cf652b0e0a677508753f
